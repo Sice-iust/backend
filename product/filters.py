235da7924b9e5c2739e8408ccaf157e6bb265112
fatemehesmaeli <asfaty200@gmail.com>
2025-06-06 05:10:33 -0700

    fix:fix filter

--- product/filters.py
+++ product/filters.py
@@ -2,9 +2,13 @@ import django_filters
 from .models import Product
 
 
+class CharInFilter(django_filters.BaseInFilter, django_filters.CharFilter):
+    pass
+
+
 class ProductFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr="icontains")
-    category = django_filters.CharFilter(field_name="category__category", lookup_expr="iexact", label="Filter by category name")
+    category = CharInFilter(field_name="category__category", lookup_expr="in", label="Filter by category names")
     box_type = django_filters.NumberFilter()
     discount = django_filters.NumberFilter(field_name="discount", lookup_expr="gt")
 
